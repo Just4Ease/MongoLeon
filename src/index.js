01ba6442faf5180dc5e974e05e427c4037d3337a
Justice Nefe <justicenefe@gmail.com>
2019-09-19 11:10:13 +0100

    Updated mongo leon to accept arguments of DBName and URL ( Database URL )

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
 const express = require('express');
 const path = require('path');
-const Config = require('./config');
 
 
-module.exports.init = (connection) => {
+/**
+ * Initialize Mongo Leon
+ * @param app
+ * @param DBName
+ * @param URL
+ * @return {app|Error}
+ */
+module.exports.init = ({ DBName, URL } = {}) => {
 	const app = express();
-	const config = new Config();
 	const router = express.Router();
 	app.disable('x-powered-by');
-	app.use('/static', express.static(path.join(config.root, './static')));
+	app.use('/static', express.static(path.normalize(path.join(__dirname, './static'))));
+	if (!DBName || !URL) {
+		app.use((req, res, next) => {
+			const err = new Error('Please provide a DBName and a URL for MongoLeon in the init argument');
+			err.status = 404;
+			return next(err);
+		});
+	}
 
 	const controller = module.require('./controllers');
-	app.use('/mongo-leon', controller(router, connection));
+	app.use('/mongo-leon', controller(router, {
+		DBName,
+		URL
+	}));
 	return app;
 };
